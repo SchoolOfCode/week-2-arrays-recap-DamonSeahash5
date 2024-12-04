f43b1a6b63e0b92293ebcc5d29fecd3ea7347823
DamonSeahash5 <oliverhopwood5@gmail.com>
2024-12-04 08:44:27 +0000

    index.html, main.js created, tasks attempted

---
+++ main.js
@@ -0,0 +1,58 @@
+//Taks 1 - Declaring an Array
+
+const favouriteFoods = ['Cheese', 'Chocolate', 'Olives', 'Anchovies',];
+
+console.log(favouriteFoods);
+
+const favouriteSongs = ['Tiny Dancer', 'Running Up That Hill', 'Don\'t Stop Moving', 'Tragedy', 'Crazy Horses',];
+
+console.log(favouriteSongs)
+
+const profit = [1, 2, 4, 8, 16, 32, 64,];
+
+console.log(profit);
+
+
+const Chris = {
+    name: "Chris",
+    specialSkill: "Changing Lives",
+}
+const Lizzie = {
+    name: "",
+    specialSkill: "",
+}
+const Ben = {}
+const Tao = {}
+const Liz = {}
+const Patrick = {};
+const Tim = {};
+const James = {};
+const Joseph = {};
+const Max = {}
+
+const friends = [Chris, Lizzie, Ben, Tao, Liz, Patrick, Tim, James, Joseph, Max,];
+
+console.log(friends);
+
+//Task 2 - Reading Options
+
+let options = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  console.log(options[6]);
+  console.log(options[4]);
+  console.log(options[6]);
+
+  
